704706cbeddb7670e519bf7f2bfe27b091cccbff
EgorSherstnev <svarog91@list.ru>
2023-08-10 01:16:25 +0300

    update taskRouter

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -8,7 +8,6 @@ router.put('/update', taskController.update)
 router.get('/get_tasks_by_list', taskController.getTasksByList);
 router.get('/get_all_tasks', taskController.getAllTasks);
 
-//router.delete('/delete', taskController.delete)
 router.delete('/delete/:unicId', taskController.delete)
 
 module.exports = router;
